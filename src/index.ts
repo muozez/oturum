d57154041ad5e90bd90d9a1b4c4a5ef37d778020
Muhammet Yılmaz <muhammetylmaz128@gmail.com>
2025-12-06 22:15:42 +0300

    feat: mongo-db docker compose file created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("heeeyoo")
+import Express = require("express");
+import 'dotenv/config';
+const port = process.env.PORT;
+const app = Express();
+
+app.use('/', (req, res)=>{
+    res.status(200);
+    res.json({message: "200"})
+})
+
+app.listen(port, ()=> console.log(`Listening at ${port}`))
