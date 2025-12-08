41eda96affefada7a43d4b50a900c0fcabacade6
Muhammet Yılmaz <muhammetylmaz128@gmail.com>
2025-12-08 23:32:28 +0300

    feat: tailwind configured & switched to tailwind css order

--- frontend/oturum-ui/src/App.tsx
+++ frontend/oturum-ui/src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
 import QRCard from './components/QRCard'
 
 function App() {
   return (
-    <div className='main-con'>
+    <div className="flex flex-col items-center justify-center h-screen w-full bg-gray-100">
       <QRCard />
+      <p className='w-full max-w-xs mt-4 text-center text-gray-500'>QR Kodu Görevliye Gösteriniz</p>
     </div>
   )
 }
--- frontend/oturum-ui/src/components/QRCard.tsx
+++ frontend/oturum-ui/src/components/QRCard.tsx
@@ -2,8 +2,13 @@ import QRCode from 'react-qr-code';
 
 function QRCard() {
   return (
-    <div className='qr-card'>
-        <QRCode value='https://github.com/muozez' size={256}></QRCode>
+    <div className='w-full max-w-xs bg-white p-6 rounded-xl shadow padding-16'>
+        <QRCode value='https://github.com/muozez'
+        size={256}
+        style={{ width: "100%", height: "100%"}}
+        viewBox='0 0 256 256'
+        ></QRCode>
+
     </div>
   )
 }
