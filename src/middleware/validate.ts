136367372f2d81ae9c68f0211a34a1cb7dd1eee7
Muhammet Yılmaz <muhammetylmaz128@gmail.com>
2025-12-10 19:23:39 +0300

    fix: fixed error handling from validate middleware

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,4 +1,4 @@
-import { z, ZodSchema } from 'zod';
+import { z, ZodError, ZodSchema, ZodIssue } from 'zod';
 import { Request, Response, NextFunction } from "express";
 
 export const validate = (schema: ZodSchema<any>)=>{
@@ -7,7 +7,14 @@ export const validate = (schema: ZodSchema<any>)=>{
             schema.parse(req.body);
             next();
         } catch(err: any){
-            return res.status(400).json({ error: err.message})
+            if (err instanceof ZodError){
+                const errors = err.issues.map(e => ({
+                    path: e.path.join('.'),
+                    message: e.message
+                }))
+                return res.status(400).json({errors})
+            }
+            return res.status(500).json({error: "Internal Server Error"})
         }
     }
 }
