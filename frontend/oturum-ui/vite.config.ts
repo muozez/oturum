66d69ca53c4f133a1c768c6293f770b7c63daddb
Muhammet Yılmaz <muhammetylmaz128@gmail.com>
2025-12-12 10:55:53 +0300

    fix: I tried, it fixed itself somehow, I'm not touching it anymore.

--- frontend/oturum-ui/vite.config.ts
+++ frontend/oturum-ui/vite.config.ts
@@ -1,3 +1,5 @@
+import path from "path"
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -8,6 +10,11 @@ export default defineConfig({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
-    }),
+    }), tailwindcss()
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "/src"),
+    }
+  }
 })
