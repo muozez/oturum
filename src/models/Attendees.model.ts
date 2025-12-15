b6812d8418ff5925280c971ff2c57c20040fc5ba
Muhammet Yılmaz <muhammetylmaz128@gmail.com>
2025-12-16 00:32:44 +0300

    refactor: modified attendees model

--- src/models/Attendees.model.ts
+++ src/models/Attendees.model.ts
@@ -2,9 +2,14 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const AttendeesSchema = new Schema({
-    sessionId: Number,
-    userId: String,
-    checkinTime: { type: Date }
+    sessionId: {type: Schema.Types.ObjectId, ref: 'Sessions', required: true},
+    userId: {type: Schema.Types.ObjectId, ref: 'Users', required: true},
+    checkinTime: { type: Date, default: Date.now },
+    checkInMethod: {
+        type: String,
+        enum: ['token', 'phone_fallback'],
+        required: true
+    }
 })
 
 export default mongoose.model("Attendees", AttendeesSchema)
