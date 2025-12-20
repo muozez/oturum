0b090e07bd7f006d09b7f9030dd577e1fe0ac633
muozez <muhammetylmaz128@gmail.com>
2025-12-21 00:24:08 +0300

    feat: added verify-scan endpoint swagger spec

--- src/routes/session.route.ts
+++ src/routes/session.route.ts
@@ -12,6 +12,26 @@ router.post('/verify-scan', verifyScan)
 *       summary: endpoint where session participants will verify via QR code
 *       tags:
 *           - verify-scan
+*       requestBody:
+*           required: true
+*           content:
+*               application/json:
+*                   schema:
+*                       type: object
+*                       required:
+*                           - userId
+*                           - sessionOrder
+*                           - checkInMethod
+*                       properties:
+*                           userId:
+*                               type: string
+*                               example: "694711bf37b100e8a09fa7c5"
+*                           sessionOrder:
+*                               type: number
+*                               example: 1
+*                           checkInMethod:
+*                               type: string
+*                               example: "token"
 *       responses:
 *           200:
 *               description: Success
